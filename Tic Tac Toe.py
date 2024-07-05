4934023141f495529662ea616f8c82a552384371
ouata <ayoub.programmierwelt@gmail.com>
2024-07-05 10:23:19 +0200

    Gewinner-Ermittlung implementieren - prüfen, ob die jeweiligen Felder mit dem selben Spielersymbol hinterlegt sind.

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -6,7 +6,7 @@ field = ["",
 
 # Definierte Variablen.
 active_player = "X"
-
+run = True
 
 
 # Spielfeld erzeugen
@@ -40,10 +40,44 @@ def change_player():
         active_player = "X"
 
 
+# Gewinner-Ermittlung implementieren - prüfen, ob die jeweiligen Felder mit dem selben Spielersymbol hinterlegt sind.
+def check_win():
+    # Zeile prüfen:
+    if field[1] == field[2] == field[3]:
+        return field[1]  # Irgend ein Element an den Funktionsaufrufer schicken, damit dieser weiß, welches Spieler-
+    # symbol auf dieses Spielerfeld liegt.
+
+    if field[4] == field[5] == field[6]:
+        return field[5]
+
+    if field[7] == field[8] == field[9]:
+        return field[9]
+
+    # Spalte prüfen:
+    if field[1] == field[4] == field[7]:
+        return field[1]
+    if field[2] == field[5] == field[8]:
+        return field[8]
+    if field[3] == field[6] == field[9]:
+        return field[9]
+
+    # Diagonale
+    if field[1] == field[5] == field[9]:
+        return field[9]
+    if field[3] == field[5] == field[7]:
+        return field[5]
+
+
 # Funktionsaufrufe - Endlosschleife: Damit die Funktionen nicht nach einem Aufruf beendet werden.
 # Spieler-Symbol: Ich weise, die von den Spielern auserwählten Felder, das Symbol des Spielers zu.
-while True:
+# Falls jemand das Spiel gewinnt, soll das Programm beendet werden - True verpacke ich in einer Variable, damit die
+# Schleife variable wird. - If-Anweisung einbauen mit einer weiteren Variable.
+while run:
     print_field()
     player_move = next_move()
     field[player_move] = active_player
+    winner = check_win()
+    if winner:  # Wenn in dieser Variable nur irgend einen Wert beinhaltet, wird diese Variable wie ein True gewertet.
+        print("Der Spieler " + winner + " hat gewonnen!")
+        run = False  # Spiel wird beendet, wenn die Schleife auf False ist und somit nicht mehr läuft.
     change_player()
