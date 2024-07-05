e91e3a2acc5d161a15cab0985e640efc9eb5dab7
ouata <ayoub.programmierwelt@gmail.com>
2024-07-05 09:55:41 +0200

    Spielzug: Spieler dürfen nur Zahlen von 1-9 eingeben.

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -9,3 +9,15 @@ def print_field():
     print(field[1] + "|" + field[2] + "|" + field[3])
     print(field[4] + "|" + field[5] + "|" + field[6])
     print(field[7] + "|" + field[8] + "|" + field[9])
+
+
+# Spielzug umsetzen - Endlosschleife - Spieler können nur Zahlen zwischen 1-9 eingeben.
+def next_move():
+    while True:
+        player_move = input("Bitte gebe eine Zahl zwischen 1-9 ein: ")
+        player_move = int(player_move)
+        if player_move >= 1 and player_move <= 9:
+            return player_move
+        else:
+            print("Die Zahl liegt nicht in dem von mir definerten Rahmenbereich")
+
