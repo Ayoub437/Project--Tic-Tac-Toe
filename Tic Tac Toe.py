461a90f2da53e1ef256e12440bfd1d9836274e8a
ouata <ayoub.programmierwelt@gmail.com>
2024-07-05 09:50:07 +0200

    Spielfeld erzeugen

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -4,3 +4,8 @@ field = ["",
          "4", "5", "6",
          "7", "8", "9"]
 
+# Spielfeld erzeugen
+def print_field():
+    print(field[1] + "|" + field[2] + "|" + field[3])
+    print(field[4] + "|" + field[5] + "|" + field[6])
+    print(field[7] + "|" + field[8] + "|" + field[9])
